945ebdb59015f19ccf09d79ede1629b27aca1b02
Roman Solar <romanNsolar@gmail.com>
2021-11-20 03:49:10 -0800

    Event handlers set

--- drawing-react-canvas/src/App.js
+++ drawing-react-canvas/src/App.js
@@ -2,23 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const canvasRef = useRef(null)
+
+  //HANDLERS:
+  const startDrawing = () => {
+
+  }
+
+  const finishDrawing = () => {
+    //asign in to mouseUp
+  }
+
+  const draw = () => {
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <canvas
+      onMouseDown = {startDrawing}
+      onMouseUp = {finishDrawing}
+      onMouseMove = {startDrawing}
+      ref = {canvasRef}
+    />
   );
 }
 
